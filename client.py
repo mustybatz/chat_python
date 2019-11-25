0b83c5c36ad493febf129d161a3b74bcdfc2ba4c
Mustybatz <barush_mendez-3.0@hotmail.com>
2019-11-25 07:22:22 -0600

    Commit inicial

---
+++ client.py
@@ -0,0 +1,42 @@
+import socket
+import sys
+import pickle
+import threading
+
+class Cliente():
+
+    def __init__(self, host="localhost", port=4000):
+        #conexión del cliente al servidor
+        self.sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.sock.connect((str(host), int(port)))
+        #Leer mensajes del servidor mediante un hilo
+        msg_recv = threading.Thread(target=self.msg_recv)
+
+        msg_recv.daemon = True
+        msg_recv.start()
+
+        #Hilo que mantendrá vivo el hilo principal y nos permitirá escribir los mensajes.
+        while True:
+            msg = input('>')
+            if msg != 'salir':
+                self.send_msg(msg)
+            else:
+                self.sock.close()
+                sys.exit()
+    #Función para recibir mensajes
+    def msg_recv(self):
+        while True:
+            try:
+                data = self.sock.recv(1024)
+                if data:
+                    print(f">{pickle.loads(data)}")
+            except:
+                pass
+    #Función para mandar mensajes
+    def send_msg(self, msg):
+        try:
+            self.sock.send(pickle.dumps(msg))
+        except:
+            print("error")
+
+c = Cliente()
---
+++ server.py
@@ -0,0 +1,68 @@
+import socket
+import pickle
+import sys
+import threading
+
+class Servidor():
+    def __init__(self, host = "localhost", port="4000"):
+
+        self.clientes = []
+
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.bind((str(host), int(port)))
+        self.sock.listen(10)
+        self.sock.setblocking(False)
+
+        #hilos para aceptar y rechazar conexiones.
+        aceptar = threading.Thread(target=self.aceptarCon)
+        procesar = threading.Thread(target=self.procesarCon)
+
+        aceptar.daemon = True
+        aceptar.start()
+
+        procesar.daemon = True
+        procesar.start()
+
+        try:
+            while True:
+                msg = input(">")
+                if msg == "salir":
+                    break
+            self.sock.close()
+            sys.exit()
+        except:
+            self.sock.close()
+            sys.exit()
+    #función para mostrar mensajes
+    def msg_to_all(self, msg, cliente):
+        for c in self.clientes:
+            try:
+                if c != cliente:
+                    c.send(msg)
+            except:
+                self.clientes.remove(c)
+    #función que acepta las conexiones y las almacena en un arreglo
+    def aceptarCon(self):
+        print("Aceptar conexión iniciado.")
+        while True:
+            try:
+                conn, addr = self.sock.accept()
+                conn.setblocking(False)
+                self.clientes.append(conn)
+            except:
+                pass
+    #función que procesa las conexiones.
+    def procesarCon(self):
+        print("Procesar conexión iniciado.")
+        while True:
+            if len(self.clientes) > 0:
+                for c in self.clientes:
+                    try:
+                        data = c.recv(1024)
+                        if data:
+                            self.msg_to_all(data,c)
+                    except:
+                        pass 
+
+#Iniciar servidor.
+c = Servidor()
